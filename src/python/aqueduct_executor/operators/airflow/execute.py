4889785cd2fcd160dc7bf352454b60adb0eaa687
Saurav Chhatrapati <sauravc@berkeley.edu>
2022-06-14 20:23:41 +0000

    Tests compile airflow job

--- src/python/aqueduct_executor/operators/airflow/execute.py
+++ src/python/aqueduct_executor/operators/airflow/execute.py
@@ -1,11 +1,24 @@
+from typing import Union
 import traceback
 import sys
+import os
 
 from aqueduct_executor.operators.airflow import spec
+from aqueduct_executor.operators.connectors.tabular import spec as conn_spec
+from aqueduct_executor.operators.function_executor import spec as func_spec
+from aqueduct_executor.operators.param_executor import spec as param_spec
 from aqueduct_executor.operators.utils import utils
 from aqueduct_executor.operators.utils.storage import parse
 
 from jinja2 import Environment, FileSystemLoader
+
+TaskSpec = Union[conn_spec.ExtractSpec, conn_spec.LoadSpec, func_spec.FunctionSpec, param_spec.ParamSpec]
+
+class Task:
+    def __init__(self, task_id: str, spec: TaskSpec, alias: str):
+        self.id = task_id
+        self.spec = spec
+        self.alias = alias
 
 def run(spec: spec.CompileAirflowSpec):
     """
@@ -16,10 +29,10 @@ def run(spec: spec.CompileAirflowSpec):
     storage = parse.parse_storage(spec.storage_config)
     try:
         compile(spec)
-        utils.write_operator_metadata(storage, spec.metadata_path, err="", logs={})
+        # utils.write_operator_metadata(storage, spec.metadata_path, err="", logs={})
     except Exception as e:
         traceback.print_exc()
-        utils.write_operator_metadata(storage, spec.metadata_path, err=str(e), logs={})
+        # utils.write_operator_metadata(storage, spec.metadata_path, err=str(e), logs={})
         sys.exit(1)
 
 def compile(spec: spec.CompileAirflowSpec) -> bytes:
@@ -28,22 +41,30 @@ def compile(spec: spec.CompileAirflowSpec) -> bytes:
     It returns the DAG file.
     """
 
-    # Init Airflow DAG id
-    dag_id = "{}-{}".format(spec.workflow_id, spec.workflow_name)
-
     # Init Airflow tasks
     tasks = []
-    for task_id, task_spec in spec.specs.items():
+    i = 0
+    for task_id, task_spec in spec.task_specs.items():
+        
         # Todo figure out dependencies
-        dependencies = []
-        t = task.Task(task_id, task_spec, dependencies)
-        tasks.append(t)
 
-    env = Environment(loader=FileSystemLoader("./"))
+        t = Task(task_id, task_spec, "t{}".format(i))
+        tasks.append(t)
+        i += 1
+
+    env = Environment(loader=FileSystemLoader("./aqueduct_executor/operators/airflow"))
+
+    print(os.getcwd())
+
     template = env.get_template("dag.template")
     r = template.render(
-        dag_id="",
+        dag_id=spec.dag_id,
+        workflow_id=spec.workflow_id,
         tasks=tasks,
         edges=spec.edges,
     )
+
+    with open('dag.py', "w") as f:
+        f.write(r)
+
     return None
