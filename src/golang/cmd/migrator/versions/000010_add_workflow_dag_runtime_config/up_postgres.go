45690e5078e0e7c46afa2794a4475c1a68670c15
Saurav Chhatrapati <sauravc@berkeley.edu>
2022-06-29 11:59:30 -0400

    [SCHEMA] - Deletes old v10

--- src/golang/cmd/migrator/versions/000010_add_workflow_dag_runtime_config/up_postgres.go
+++
@@ -1,6 +0,0 @@
-package _000010_add_workflow_dag_runtime_config
-
-const upPostgresScript = `
-ALTER TABLE workflow_dag
-ADD COLUMN runtime_config JSONB NOT NULL
-DEFAULT '{"type":"aqueduct", "aqueductConfig":{}}'::jsonb;`
--- src/golang/cmd/migrator/versions/000010_add_workflow_dag_runtime_config/up_sqlite.go
+++
@@ -1,8 +0,0 @@
-package _000010_add_workflow_dag_runtime_config
-
-const sqliteScript = `
-ALTER TABLE workflow_dag
-ADD COLUMN runtime_config BLOB
-DEFAULT '{"type":"aqueduct", "aqueductConfig":{}}'
-NOT NULL;
-`
